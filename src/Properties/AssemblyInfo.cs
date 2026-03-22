babb109d4c3a17a6e05c886a6451bdfd67123c40
validaq <validaq@gmail.com>
2026-03-23 00:14:10 +0100

    Update assembly version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ValidAQ")]
 [assembly: AssemblyProduct("QM_CloneSort")]
 [assembly: AssemblyCopyright("Copyright © ValidAQ")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
